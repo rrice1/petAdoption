e8a585cec4d1f17899a5899c96d82ab13f8bc9af
Robert Rice <rrice1291@gmail.com>
2018-10-06 13:48:52 -0500

    pretty much finished styling

--- javascripts/components/petComponent.js
+++ javascripts/components/petComponent.js
@@ -27,24 +27,39 @@ const sortPets = (e) => {
 
 };
 
+
+
 let petsBuilder = (petsArray) => {
     let newString = '';
     for(let i=0; i<petsArray.length;i++){
-    newString += `<div class="col-2 pet-card" id="${petsArray[i].name}">`
-    newString+= `<h4 class="card-header">${petsArray[i].name}</h4>`;
+    newString += `<div class="col-4 pet-card container-card" id="${petsArray[i].name}">`
+    newString+= `<h4 class="card-header d-flex justify-content-center text-secondary bg-dark">${petsArray[i].name}</h4>`;
     newString+= `<div class="card">`;
     
     newString+= `<img class="card-img-top" src="${petsArray[i].imageUrl}" alt="">`;
-    newString+= `<div class="card-body">`;
+    newString+= `<div class="card-body d-flex justify-content-center">`;
     newString+= `<p class="card-title">${petsArray[i].specialSkill}</p>`;
     newString+= `</div>`;
     newString+= `</div>`;
-    newString+= `<p class="card-footer">${petsArray[i].type}</p>`;
+    newString+= `<p id="footerid${[i]}" class="card-footer d-flex justify-content-center hello">${petsArray[i].type}</p>`;
     newString+= `</div>`;
-    
+
 }
+
 printToDom(newString,'pets');
+colorCode();
 }
 
-
+let colorCode = () =>{
+    for(let i=0; i<pets.length;i++){
+        if(pets[i].type==='cat'){
+            document.getElementById(`footerid${[i]}`).classList.toggle('cat')
+        } else if (pets[i].type==='dog'){
+            document.getElementById(`footerid${[i]}`).classList.toggle('dog')
+        } else if (pets[i].type==='dino'){
+            document.getElementById(`footerid${[i]}`).classList.toggle('dino')
+        }
+    }
+}
+        
 export {petsBuilder,setPets,getPetsz,sortPets}
